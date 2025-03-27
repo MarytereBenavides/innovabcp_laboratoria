848d76cfec4248452cdc96f41be38f03ecbca600
Barbara <0deabreubarbara@gmail.com>
2025-03-27 18:01:04 -0500

    feat: trivia setup and endpoints

---
+++ backend/src/controllers/triviaController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from 'express';
+import * as triviaService from '../services/triviaService';
+
+export const startGame = async (req: Request, res: Response) => {
+  try {
+    const { user_id } = req.body;
+    const gameSession = await triviaService.startTrivia(user_id);
+
+    res.json({ message: 'Trivia iniciada', gameSession });
+  } catch (error) {
+    res.status(500).json({ error: 'Error iniciando trivia' });
+  }
+};
+
+export const getQuestion = async (req: Request, res: Response) => {
+  try {
+    const { game_session_id } = req.params;
+
+    const gameSession = await triviaService.getGameSession(+game_session_id);
+    if (!gameSession){
+        res.status(404).json({ error: 'Sesión no encontrada' });
+        return;
+    }
+
+    const questions = await triviaService.getQuestions(+gameSession.game_id);
+    if (!questions){
+        res.status(404).json({ error: 'Preguntas no encontradas' });
+        return;
+    }
+
+    res.json({
+        message: 'Preguntas obtenidas correctamente',
+        questions
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error obteniendo pregunta' });
+  }
+};
+
+export const submitAnswer = async (req: Request, res: Response) => {
+  try {
+    const { game_session_id, answer_id } = req.body;
+    const result = await triviaService.checkAnswer(game_session_id, answer_id);
+
+    if (!result){
+        res.status(404).json({ error: 'Respuesta no encontrada' });
+        return
+    }
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Error procesando respuesta' });
+  }
+};
+
+export const endGame = async (req: Request, res: Response) => {
+  try {
+    const { game_session_id, score } = req.body;
+    const gameSession = await triviaService.getGameSession(+game_session_id);
+    if (!gameSession){
+        res.status(404).json({ error: 'Sesión no encontrada' });
+        return;
+    }
+
+    await triviaService.updateScore(+game_session_id, score);
+        
+    res.json({ message: 'Trivia finalizada' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error finalizando trivia' });
+  }
+};
---
+++ backend/src/services/triviaService.ts
@@ -0,0 +1,58 @@
+import prisma from '../config/database';
+
+export const startTrivia = async (userId: number) => {
+  //TODO: Validar si ya existe una sesion activa devolverla y no crear otra nueva
+  return await prisma.game_session.create({
+    data: {
+      user_id: userId,
+      game_id: 1,
+      earned_xp: 0,
+      earned_coins: 0,
+      score: 0
+    }
+  });
+};
+
+export const getGameSession = async (gameSessionId: number) => {
+  return await prisma.game_session.findUnique({
+    where: { game_session_id: gameSessionId }
+  });
+}
+
+export const getQuestions = async (gameId: number) => {
+  return await prisma.mini_game_question.findMany({
+    where: { game_id: gameId },
+    include: { answers: true }
+  });
+};
+
+export const checkAnswer = async (gameSessionId: number, answerId: number) => {
+  const answer = await prisma.mini_game_answer.findUnique({
+    where: { answer_id: answerId }
+  });
+
+  if (!answer) return null;
+
+  const isCorrect = answer.is_correct;
+  let scoreUpdate = isCorrect ? 10 : 0;
+
+  await prisma.game_session.update({
+    where: { game_session_id: gameSessionId },
+    data: { score: { increment: scoreUpdate } }
+  });
+
+  return { isCorrect, scoreEarned: scoreUpdate };
+};
+
+export const updateScore = async (gameSessionId: number, score: number) => {
+  return await prisma.game_session.update({
+    where: { game_session_id: gameSessionId },
+    data: { score: { increment: Number(score) } }
+  });
+};
+
+export const endTrivia = async (gameSessionId: number) => {
+  return await prisma.game_session.findUnique({
+    where: { game_session_id: gameSessionId }
+  });
+};
