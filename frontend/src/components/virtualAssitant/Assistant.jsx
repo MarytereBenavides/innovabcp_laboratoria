663f731410adf28e42b72a6c4e8730295bce026f
mbenavides <maryterebenavides@gmail.com>
2025-03-28 05:53:32 -0500

    feat: ia asisstant

--- frontend/src/components/virtualAssitant/Assistant.jsx
+++ frontend/src/components/virtualAssitant/Assistant.jsx
@@ -1,16 +1,164 @@
-import { useGLTF } from "@react-three/drei"
+import { teachers,useAITeacher } from "@/hooks/useAIAssistant";
+import { Html, useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { MathUtils, MeshStandardMaterial } from "three";
+import { randInt } from "three/src/math/MathUtils";
 
-export const Assistants=["Nanami"]
+const ANIMATION_FADE_TIME = 0.5;
 
 export const Assistant = ({assistant, ...props}) => {
+    const group = useRef();
     const {scene} = useGLTF(`/models/Assistant_${assistant}.glb`)
+    useEffect(() => {
+        scene.traverse((child) => {
+          if (child.material) {
+            child.material = new MeshStandardMaterial({
+              map: child.material.map,
+            });
+          }
+        });
+      }, [scene]);
+
+    const currentMessage = useAITeacher((state) => state.currentMessage);
+  const loading = useAITeacher((state) => state.loading);
+  const { animations } = useGLTF(`/models/animations_${assistant}.glb`);
+  const { actions, mixer } = useAnimations(animations, group);
+  const [animation, setAnimation] = useState("Idle");
+
+  // Imported from r3f-virtual-girlfriend project
+  const [blink, setBlink] = useState(false);
+
+  useEffect(() => {
+    let blinkTimeout;
+    const nextBlink = () => {
+      blinkTimeout = setTimeout(() => {
+        setBlink(true);
+        setTimeout(() => {
+          setBlink(false);
+          nextBlink();
+        }, 100);
+      }, randInt(1000, 5000));
+    };
+    nextBlink();
+    return () => clearTimeout(blinkTimeout);
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      setAnimation("Thinking");
+    } else if (currentMessage) {
+      setAnimation(randInt(0, 1) ? "Talking" : "Talking2");
+    } else {
+      setAnimation("Idle");
+    }
+  }, [currentMessage, loading]);
+
+  const lerpMorphTarget = (target, value, speed = 0.1) => {
+    console.log('entre a letpMorphTarget', target, value, speed);
+    scene.traverse((child) => {
+      if (child.isSkinnedMesh && child.morphTargetDictionary) {
+        console.log(`Morph targets de ${child.name}:`, Object.keys(child.morphTargetDictionary),'respiestar');
+        const index = child.morphTargetDictionary[target];
+        if (
+          index === undefined ||
+          child.morphTargetInfluences[index] === undefined
+        ) {
+          console.warn(`Morph target "${target}" not found in`, child.name);
+          return;
+        }
+        child.morphTargetInfluences[index] = MathUtils.lerp(
+          child.morphTargetInfluences[index],
+          value,
+          speed
+        );
+        console.log('sin error');
+      }
+    });
+    
+  };
+
+  useFrame(({ camera }) => {
+    // Smile
+    lerpMorphTarget("mouthSmile", 0.2, 0.5);
+    // Blinking
+    lerpMorphTarget("eye_close", blink ? 1 : 0, 0.5);
+
+    // Talking
+    for (let i = 0; i <= 21; i++) {
+      lerpMorphTarget(i, 0, 0.1); // reset morph targets
+    }
+    console.log('currentMessage',currentMessage);
+    if (
+      currentMessage &&
+      currentMessage.visemes &&
+      currentMessage.audioPlayer
+    ) {
+      for (let i = currentMessage.visemes.length - 1; i >= 0; i--) {
+        const viseme = currentMessage.visemes[i];
+        if (currentMessage.audioPlayer.currentTime * 1000 >= viseme[0]) {
+          console.log('tengo visemes', viseme[1]);
+          lerpMorphTarget(viseme[1], 1, 0.2);
+          break;
+        }
+      }
+      if (
+        actions[animation].time >
+        actions[animation].getClip().duration - ANIMATION_FADE_TIME
+      ) {
+        setAnimation((animation) =>
+          animation === "Talking" ? "Talking2" : "Talking"
+        ); // Could load more type of animations and randomization here
+      }
+    }
+  });
+
+  useEffect(() => {
+    actions[animation]
+      ?.reset()
+      .fadeIn(mixer.time > 0 ? ANIMATION_FADE_TIME : 0)
+      .play();
+    return () => {
+      actions[animation]?.fadeOut(ANIMATION_FADE_TIME);
+    };
+  }, [animation, actions]);
+
+  
+  const [thinkingText, setThinkingText] = useState(".");
+
+  useEffect(() => {
+    if (loading) {
+      const interval = setInterval(() => {
+        setThinkingText((thinkingText) => {
+          if (thinkingText.length === 3) {
+            return ".";
+          }
+          return thinkingText + ".";
+        });
+      }, 500);
+      return () => clearInterval(interval);
+    }
+  }, [loading]);
     return (
-        <group {...props}>
+        <group {...props} dispose={null} ref={group}>
+              {loading && (
+        <Html position-y={assistant === "Nanami" ? 1.6 : 1.8}>
+          <div className="flex justify-center items-center -translate-x-1/2">
+            <span className="relative flex h-8 w-8 items-center justify-center">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+              <span className="relative inline-flex items-center justify-center duration-75 rounded-full h-8 w-8 bg-white/80">
+                {thinkingText}
+              </span>
+            </span>
+          </div>
+        </Html>
+      )}
             <primitive object={scene} />
         </group>
     )
 }
 
-Assistants.forEach((assistant) => {
-    useGLTF.preload(`/models/Assitant_${assistant}.glb`)
+teachers.forEach((assistant) => {
+    useGLTF.preload(`/models/Assistant_${assistant}.glb`);
+    useGLTF.preload(`/models/animations_${assistant}.glb`);
 })
--- frontend/src/components/virtualAssitant/Experience.jsx
+++ frontend/src/components/virtualAssitant/Experience.jsx
@@ -1,60 +1,137 @@
 "use client";
-import {Box, Environment, Gltf, OrbitControls, CameraControls} from '@react-three/drei';
-import { Canvas,invalidate,useFrame   } from '@react-three/fiber';
-import { Suspense, useRef } from 'react';
+import {
+    CameraControls,
+    Environment,
+    Float,
+    Gltf,
+    Html,
+    Loader,
+    useGLTF,
+  } from "@react-three/drei";
+import { Canvas } from '@react-three/fiber';
+import { Leva, button, useControls } from "leva";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { degToRad } from "three/src/math/MathUtils";
 import { Assistant } from './Assistant';
-import { degToRad } from 'three/src/math/MathUtils';
+import { TypingBox } from './TypingBox';
+import { MessagesList } from './MessageList';
+import { useAITeacher } from "@/hooks/useAIAssistant";
+
+
+const itemPlacement = {
+    default: {
+      classroom: {
+        position: [0.2, -1.7, -2],
+      },
+      teacher: {
+        position: [-1, -1.7, -3],
+      },
+      board: {
+        position: [0.45, 0.382, -6],
+      },
+    }
+  };
 
 export const Experience = () => {   
+    const teacher = useAITeacher((state) => state.teacher);
+    const classroom = useAITeacher((state) => state.classroom);
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+      }, []);
+
+    
+    if (!isMounted) {
+       return <Loader />;
+    }
     return(
         <>
+            <div className="z-10 md:justify-center fixed bottom-4 left-4 right-4 flex gap-3 flex-wrap justify-stretch">
+                <TypingBox />
+            </div>
+            <Leva hidden />
+            <Loader />
             <Canvas 
             camera={{ position: [0, 0, 0.0001], fov: 50 }} 
-            frameloop="demand"
-            dpr={[1, 1.2]} 
-            gl={{ powerPreference: "low-power" }}    
-            performance={{ min: 0.5 }}  
+           frameloop="demand"
+           dpr={1}
+           gl={{ powerPreference: "high-performance" }}
+           performance={{ min: 0.1 }}
             shadows={false} >
+                 <CameraManager />
                 <Suspense fallback={null}>
-                    <Environment preset="sunset" />
-                    <ambientLight intensity={0.5} color="pink" />
-                    <Assistant assistant={"Nanami"} position={[-1, -1.7, -3]} scale={1.5} rotation-y={degToRad(20)} />
-                    <GltfModel />
+                    <Float speed={0.5} floatIntensity={0.2} rotationIntensity={0.1}>
+                        <Html transform {...itemPlacement[classroom].board} distanceFactor={1}>
+                            <MessagesList />
+                        </Html>
+                        <Environment preset="sunset" />
+                        <ambientLight intensity={0.3} color="pink" />
+                        <Assistant assistant={teacher}    key={teacher}    {...itemPlacement[classroom].teacher} scale={1.5} rotation-y={degToRad(20)} />
+                        <Gltf  src={`/models/classroom_${classroom}.glb`}   {...itemPlacement[classroom].classroom} dracoCompression receiveShadow={false} castShadow={false}  />
+                    </Float>
                 </Suspense>
-               <CameraManager />
+              
                
             </Canvas>
         </>
     )
 }
 
-const GltfModel = () => {
-    return <Gltf src="/models/classroom_default.glb" position={[0.2, -1.7, -2]} dracoCompression/>;
-};
+const CAMERA_POSITIONS = {
+    default: [0, 6.123233995736766e-21, 0.0001],
+    loading: [
+      0.00002621880610890309, 0.00000515037441056466, 0.00009636414192870058,
+    ],
+    speaking: [0, -1.6481333940859815e-7, 0.00009999846226827279],
+  };
+  
+  const CAMERA_ZOOMS = {
+    default: 1,
+    loading: 1.3,
+    speaking: 2.1204819420055387,
+  };
 
 const CameraManager = () => {
-    const cameraControlsRef = useRef();
-    useFrame(() => {
-        if (cameraControlsRef.current) {
-          cameraControlsRef.current.update(0.1);
-        }
-      });
+    const controls = useRef();
+    const loading = useAITeacher((state) => state.loading);
+    const currentMessage = useAITeacher((state) => state.currentMessage);
+
+     useEffect(() => {
+    if (loading) {
+      controls.current?.setPosition(...CAMERA_POSITIONS.loading, true);
+      controls.current?.zoomTo(CAMERA_ZOOMS.loading, true);
+    } else if (currentMessage) {
+      controls.current?.setPosition(...CAMERA_POSITIONS.speaking, true);
+      controls.current?.zoomTo(CAMERA_ZOOMS.speaking, true);
+    }
+    }, [loading, currentMessage]);
+
+    useControls("Helper", {
+        getCameraPosition: button(() => {
+        const position = controls.current.getPosition();
+        const zoom = controls.current.camera.zoom;
+        console.log([...position], zoom);
+        }),
+    });
+
     return (
         <CameraControls 
-            ref={cameraControlsRef}
-            makeDefault
-            minDistance={0.1} // Distancia mínima al hacer zoom
-            maxDistance={0.5} // Distancia máxima al alejarse
-            minZoom={0.1}
+            ref={controls}
+            minZoom={1}
             maxZoom={3}
-            zoomSpeed={1} // Velocidad del zoom con la rueda
-            polarRotateSpeed={-0.3}
-            azimuthRotateSpeed={-0.3}
-            maxAzimuthAngle={Math.PI / 4} // Limita la rotación en azimut
-            minPolarAngle={Math.PI / 4} // Evita que mire demasiado abajo
-            maxPolarAngle={Math.PI / 2} // Evita que la cámara mire hacia abajo
-            mouseButtons={{ left:1, wheel:16 }}
-            touches={{ one:32, two:512 }}
+            polarRotateSpeed={-0.5} // Aumenta la velocidad para más respuesta
+            azimuthRotateSpeed={-0.5} // Aumenta la velocidad para más respuesta
+            smoothTime={0.05}
+            mouseButtons={{
+            left: 1, //ACTION.ROTATE
+            wheel: 16, //ACTION.ZOOM
+            }}
+            touches={{
+            one: 32, //ACTION.TOUCH_ROTATE
+            two: 512, //ACTION.TOUCH_ZOOM
+            }}
         />
     )
 }
+useGLTF.preload("/models/classroom_default.glb");
